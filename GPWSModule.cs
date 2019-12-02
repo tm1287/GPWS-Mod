ce9dd20d0fda42bd8a38e1eff64ad120e65156d4
tm1287 <tejas.maraliga@gmail.com>
2019-12-02 16:17:15 -0500

    Initial Commit; Add Basic Structure for GPWSModule Class

---
+++ GPWSModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace GPWS
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class GPWSModule : MonoBehaviour {
+        public void Start() { 
+        }
+
+        public void Update() { 
+        }
+    }
+}
