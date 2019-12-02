18ab5b4e1e9fe91b20e2409cc9fe42577cb030fc
tm1287 <tejas.maraliga@gmail.com>
2019-12-02 16:45:51 -0500

    Add backbone for SoundManager Class

---
+++ SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace GPWS {
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    class SoundManager: MonoBehaviour {
+        public void Start() {
+        }
+
+        public void Update() {
+        }
+    }
+}
