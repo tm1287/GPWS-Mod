954536ed791071f4c8f297d13bbd44f4b838110f
tm1287 <tejas.maraliga@gmail.com>
2019-12-03 11:00:50 -0500

    Add Sink and Terrain Modes to GPWS Class

--- GPWSModule.cs
+++ GPWSModule.cs
@@ -5,10 +5,46 @@ namespace GPWS
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class GPWSModule : MonoBehaviour {
-        public void Start() { 
+
+        public Vessel activeVessel;
+        private enum GPWSMode {Nominal, BankAngle, RapidSink, Stall, TerrainPullUp};
+        private GPWSMode mode;
+        private SoundManager sm; 
+        public void Start() {
+            sm = gameObject.GetComponent<SoundManager>() ?? gameObject.AddComponent<SoundManager>();
         }
 
-        public void Update() { 
+        public void Update() {
+            activeVessel = FlightGlobals.ActiveVessel;
+            
+            if (activeVessel.radarAltitude >= 150 && activeVessel.verticalSpeed < -40) {
+                mode = GPWSMode.RapidSink;
+            } else if (activeVessel.radarAltitude < 150 && activeVessel.verticalSpeed < -10) {
+                mode = GPWSMode.TerrainPullUp;
+            } 
+            else {
+                mode = GPWSMode.Nominal;
+            }
+
+            switch (mode) {
+                case GPWSMode.Nominal:
+                    //TODO: Implement some kind of prediction system during nominal operation.
+                    break;
+                case GPWSMode.BankAngle:
+                    sm.PlaySound(sm.bankAngleWarning, 1.0f);
+                    break;
+                case GPWSMode.RapidSink:
+                    sm.PlaySound(sm.dontSinkWarning, 1.0f);
+                    break;
+                case GPWSMode.Stall:
+                    sm.PlaySound(sm.stallWarning, 1.0f);
+                    break;
+                case GPWSMode.TerrainPullUp:
+                    sm.PlaySound(sm.terrainPullUpWarning, 1.0f);
+                    break;
+            }
+
+            //Debug.Log(Vector3d.Angle(activeVessel.srf_vel_direction, (Vector3d) activeVessel.terrainNormal));
         }
     }
 }
--- SoundManager.cs
+++ SoundManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 namespace GPWS {
     [KSPAddon(KSPAddon.Startup.MainMenu, false)]
     class SoundManager: MonoBehaviour {
-        public static AudioClip bankAngleWarning, dontSinkWarning, stallWarning, terrainPullUpWarning;
-        public static AudioSource audioSource;
-        public void Start() {
-            bankAngleWarning = GameDatabase.Instance.GetAudioClip("PullUpWarning/Sounds/BankAngleWarning");
-            dontSinkWarning = GameDatabase.Instance.GetAudioClip("PullUpWarning/Sounds/DontSinkWarning");
-            stallWarning = GameDatabase.Instance.GetAudioClip("PullUpWarning/Sounds/StallWarning");
-            terrainPullUpWarning = GameDatabase.Instance.GetAudioClip("PullUpWarning/Sounds/TerrainPullUpWarning");
+        public AudioClip bankAngleWarning, dontSinkWarning, stallWarning, terrainPullUpWarning;
+        public AudioSource audioSource;
+        
+    public void Start() {
+            bankAngleWarning = GameDatabase.Instance.GetAudioClip("GPWS/Sounds/BankAngleWarning");
+            dontSinkWarning = GameDatabase.Instance.GetAudioClip("GPWS/Sounds/DontSinkWarning");
+            stallWarning = GameDatabase.Instance.GetAudioClip("GPWS/Sounds/StallWarning");
+            terrainPullUpWarning = GameDatabase.Instance.GetAudioClip("GPWS/Sounds/TerrainPullUpWarning");
 
             if (bankAngleWarning == null) {
                 Debug.LogError("bankAngleWarning Sound not found.");
@@ -23,6 +24,7 @@ namespace GPWS {
             }
 
             audioSource = gameObject.GetComponent<AudioSource>() ?? gameObject.AddComponent<AudioSource>();
+
         }
 
         public void Update() {
