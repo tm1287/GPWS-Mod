22ca435224fa92ab891a01bd1979870ac2ca42ee
tm1287 <tejas.maraliga@gmail.com>
2019-12-03 20:50:45 -0500

    Add Toggle fields to GPWSWindow class

--- GPWSUI/GPWSWindow.cs
+++ GPWSUI/GPWSWindow.cs
@@ -5,6 +5,18 @@ using UnityEngine.UI;
 
 namespace GPWSUI
 {
+    [RequireComponent(typeof(RectTransform))]
     public class GPWSWindow: MonoBehaviour{
+        [SerializeField]
+        private Toggle stallToggle = null;
+        [SerializeField]
+        private Toggle bankToggle = null;
+        [SerializeField]
+        private Toggle descentToggle = null;
+        [SerializeField]
+        private Toggle terrainToggle = null;
+
     }
+
+
 }
